c26c609ae8634b3f55eeb15625639295a2508b70
SantoshVerma97 <santosh.v@redefinealgo.com>
2024-03-16 15:23:15 +0530

    ft. team manager validation multiselect

--- src/components/user-management/team/validationSchema.js
+++ src/components/user-management/team/validationSchema.js
@@ -2,7 +2,8 @@ import * as yup from "yup";
 export const validationSchema = yup.object({
     name: yup.string("Enter Team Name").required("Enter Team Name"),
     officeIds: yup.array().of(yup.string()).min(1, "Select at least one office").required("Select Office"),
-    managerIds: yup.string("Select Manager").required("Select Manager"),
+    managerIds: yup.array().of(yup.string()).min(1, "Select at least one Manager").required("Select Manager"),
+    //managerIds: yup.array().of("Select Manager").required("Select Manager"),
     shiftType: yup.string("Select Shift Type").required("Select Shift Type"),
     description: yup.string("Enter Description").required("Enter Description"),
     sendNotification: yup.string("Enter Notification Type").required("Enter Notification Type"),
