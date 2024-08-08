e836884081c524d6814dfd48de79e727a7583d1c
SaifaliCodes <saifali@redefinealgo.com>
2024-08-08 19:02:36 +0530

    helpdesk module

---
+++ src/pages/helpdesk/change-request-module.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import helpdesk from '@/layouts/helpdesk'
+
+const MainComponent = () => {
+  return (
+    <div>Change Request Module</div>
+  )
+}
+
+export default helpdesk(MainComponent)
---
+++ src/pages/helpdesk/trip-feedback.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import helpdesk from '@/layouts/helpdesk'
+
+const MainComponent = () => {
+  return (
+    <div>Trip Feedback</div>
+  )
+}
+
+export default helpdesk(MainComponent)
